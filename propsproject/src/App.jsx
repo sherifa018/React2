1c85c3599b62327ded24a6a71b49b38198172d6c
sherifa <sherifaalsaiqe@gmail.com>
2024-11-13 12:05:57 -0500

    first comment

---
+++ propsproject/src/App.jsx
@@ -0,0 +1,27 @@
+import Card from "./Card";
+function App() {
+  const cards = [
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Card 1",
+      description: "This is the description for card 1.",
+    },
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Card 2",
+      description: "This is the description for card 2.",
+    },
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Card 3",
+      description: "This is the description for card 3.",
+    },
+  ];
+  return (
+    <div>
+      <Card cards={cards} />
+    </div>
+  );
+}
+
+export default App;
